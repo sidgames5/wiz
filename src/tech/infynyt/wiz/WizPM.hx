2c6a2e73ed181dd0fb738fec4fcd143903561de4
Sid <saytleader@outlook.com>
2022-05-27 09:48:01 -0400

    Update WizPM.hx

--- src/tech/infynyt/wiz/WizPM.hx
+++ src/tech/infynyt/wiz/WizPM.hx
@@ -6,7 +6,19 @@ class WizPM {
 	public static function main():Void {
 		if (args.length == 0) {
 			Console.log("Usage: wiz <command> [args]");
+			return;
 		}
+		
+		switch (args[0]) {
+			case "install":
+				var plist = args.slice(1);
+				if (plist.length == 0) {
+					Console.log("Usage: wiz install [packages]");
+					return;
+				}
+				break;
+		}
+		
 		return;
 	}
 }
